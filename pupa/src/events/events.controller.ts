8339d869bba98967dfd1172efe41ba8d556313b2
Leto D-Art <goncharoff.k2018@yandex.ru>
2022-11-26 13:27:11 +0300

    event management and created quiz model

---
+++ pupa/src/events/events.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { FullEvent } from 'src/dtos/event.dto';
+import { EventsService } from './events.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+@Controller('events')
+export class EventsController {
+  constructor(private readonly eventService: EventsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('')
+  async createEvent(@Body() event: FullEvent): Promise<FullEvent> {
+    return this.eventService.createEvent(event);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':code')
+  async getEventListeners(@Param('code') code: string): Promise<FullEvent[]> {
+    return this.eventService.getEvent(code);
+  }
+}
