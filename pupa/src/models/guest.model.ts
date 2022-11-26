1af0593212b01a7efe8bf3d89ee8cc6d830ef861
Leto D-Art <goncharoff.k2018@yandex.ru>
2022-11-26 05:16:04 +0300

    fixed bugs

--- pupa/src/models/guest.model.ts
+++ pupa/src/models/guest.model.ts
@@ -1,21 +1,18 @@
 import {
   AllowNull,
   Column,
-  Default,
   IsUUID,
   Model,
   PrimaryKey,
   Table,
   Unique,
 } from 'sequelize-typescript';
-import { v4 as uuidv4 } from 'uuid';
 
 @Table({
   tableName: 'guest',
 })
 export class Guest extends Model {
   @IsUUID(4)
-  @Default(uuidv4())
   @PrimaryKey
   @Unique
   @AllowNull(false)
