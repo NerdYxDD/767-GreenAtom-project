37a8a86a7c8ffa98f0963c3a591b77e93070438e
NerdYxDD <sertakov@sfedu.ru>
2022-11-26 22:38:40 +0300

    added statistics in Admin Page in Event Cards

--- lupa/src/pages/AdminPage/AdminPage.tsx
+++ lupa/src/pages/AdminPage/AdminPage.tsx
@@ -7,22 +7,27 @@ const data = [
   {
     name: 'Event1',
     desc: 'desc1',
+    statistic: '1',
   },
   {
     name: 'Event2',
     desc: 'desc2',
+    statistic: '2',
   },
   {
     name: 'Event3',
     desc: 'desc3',
+    statistic: '3',
   },
   {
     name: 'Event4',
     desc: 'desc4',
+    statistic: '4',
   },
   {
     name: 'Event5',
     desc: 'desc5',
+    statistic: '5',
   },
 ];
 
@@ -30,7 +35,13 @@ const AdminPage = () => {
   return (
     <div className={styles.AdminPageContainer}>
       {data.length > 0 &&
-        data.map((value) => <EventCard name={value.name} desc={value.desc} />)}
+        data.map((value) => (
+          <EventCard
+            name={value.name}
+            desc={value.desc}
+            statistic={value.statistic}
+          />
+        ))}
     </div>
   );
 };
