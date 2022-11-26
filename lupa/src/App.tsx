a5fa3a76c1c10dc4300f599673d436c14496e577
NerdYxDD <sertakov@sfedu.ru>
2022-11-26 03:00:04 +0300

    added initial style of Form

--- lupa/src/App.tsx
+++ lupa/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './pages/Layout/Layout';
-import Login from './pages/Login/Login';
+// import Login from './pages/Login/Login';
+// import FormPage from './pages/FormPage/FormPage';
+import { Form } from './pages/FormPage/components/Form';
 
 const App = () => (
   <div className='App'>
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route index element={<Login />} />
+
+        <Route index element={<Form />} />
+          
       </Route>
     </Routes>
   </div>
